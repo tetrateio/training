6ca435c2dd8395e69e3bec29663b01d83e4945c6
Liam White <liam@tetrate.io>
2019-02-06 08:14:21 +0000

    Add modernbank kube ndeploy infra
    Signed-off-by: Liam White <liam@tetrate.io>

--- samples/modernbank/scripts/buildBinaries.sh
+++ samples/modernbank/scripts/buildBinaries.sh
@@ -11,7 +11,7 @@ do
     SERVICE_NAME=${SERVICE_DIR##*/}
     BUILD_DIR=${SERVICE_DIR}/build/bin
     cd ${SERVICE_DIR}
-    prettyPrint "Building ${SERVICE_NAME} binary!"
+    prettyPrint "Building ${SERVICE_NAME} binary"
     GOOS=linux go build \
 		-a --ldflags '-extldflags "-static"' \
 		-o ${BUILD_DIR}/${SERVICE_NAME}-server-static ./cmd/${SERVICE_NAME}-server
