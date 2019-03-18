8bd34d5a43d8aede9c3182792e99c493157e0f4d
Dhi Aurrahman <dio@tetrate.io>
2019-03-18 08:00:08 -0700

    Allow to run with proxy pass or not
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- samples/modernbank/microservices/ui/entrypoint.sh
+++ samples/modernbank/microservices/ui/entrypoint.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-# we don't want to replace undefined env vars.
-envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" \
-    < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+if [[ -z "${API_HOST}" ]]; then
+    echo "running without proxying..."
+    cp /etc/nginx/conf.d/nginx.conf.template /etc/nginx/conf.d/default.conf
+else
+    # We don't want to replace undefined env vars.
+    envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" \
+        < /etc/nginx/conf.d/nginx-with-proxy-pass.conf.template > /etc/nginx/conf.d/default.conf
+fi
 
 exec nginx -g 'daemon off;'
